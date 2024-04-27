6549e22acf77f333f74b804712d38894969941ce
Neal L <rlawton.cs@gmail.com>
2024-04-26 23:32:31 -0700

    Updated model instructions to make them dynamic.  Added exception handling for harmful category prompts.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
+from google.generativeai import types
 import os
 from config import GOOGLE_API_KEY
 
@@ -13,22 +14,57 @@ CORS(app)
 API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=API_KEY)
 
-instruction = "You believe you are Spiderman.  You respond to all questions and statements made by the user as Spiderman would, in the first person.  You use your knowledge of Spiderman and dialogue from his comic books and movies to formulate your responses."
-model = genai.GenerativeModel("gemini-1.5-pro-latest", system_instruction=instruction)
-chat = model.start_chat(history=[])
+
+instruction, model, chat = (None, None, None)
+
+
 
 @app.route('/api/ask-gemini', methods=['POST'])
 def ask_gemini():
-    # Parse the request.
+    
+    global instruction, model, chat
+    
+    # Parse the JSON data from the front end.
     data = request.get_json()
+    
+    # Print statement for debugging purposes.
+    print("Data from front end parsed.")
+    
     # Extract the name and message.
     name = data.get('name')
+    print(name)
     message = data.get('message')
+    
+    # Print statement for debugging purposes.
+    print("Setting up Gemini model.")
+    
+    # Model setup.
+    if instruction == None:
+        instruction = f"You are {name}. You will respond to all questions and statements by the user as if you are {name}. You use your knowledge of who {name} is and what they do in order to frame your responses, in the first person. You even talk like {name}, using dialogue from their appearances in popular culture (television, movies, books, graphic novels, etc.) to form your sentences.  Limit your response to 1 paragraph." 
+        print(instruction)
+    if model == None:
+        model = genai.GenerativeModel("gemini-1.5-pro-latest", system_instruction=instruction)
+    if chat == None:
+        chat = model.start_chat(history=[])
 
+    # Print statement for debugging purposes.
+    print("Sending user's statement to Gemini.")
+    
     # Send user's message to Gemini and store its response.
-    gemini_response = chat.send_message(message)
+    try:
+        gemini_response = chat.send_message(message)
+    
+        # Print statement for debugging purposes.
+        print("Received Gemini's response.")
+        print(gemini_response.text)
+        
+        return jsonify({'response': gemini_response.text})
+    
+    except types.generation_types.StopCandidateException as e:
+        default_message = "My <blank> has advised me not to remark on that topic."
+    
+        return jsonify({'response': default_message})
 
-    return jsonify({'response': gemini_response.text})
-
+        
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
